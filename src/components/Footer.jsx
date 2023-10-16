413a20779b23bb8062da19f99dfd4828cda2392b
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-16 23:58:51 +0200

    Fix an error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,9 +35,9 @@ const Footer = () => {
       </ul>
 
       <p>
-        &copy; <span id='date'>{date}</span> Mohamed Hassan Noor.
+        &copy; <span id='date'>{date}</span> Mohamed Hassan Noor.all rights
+        reserved
       </p>
-      <p>all rights reserved</p>
     </footer>
   );
 };
