96ed35ff04efb1dd320226e48eb08b268dd7a7c0
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-17 15:51:12 +0200

    Update About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,19 +13,44 @@ const About = () => {
             <div className='underline'></div>
           </div>
           <p>
-            Hi, my name is Mohamed Hassan Noor. I am an experienced Full Stack
-            developer, committed to deliver a high-quality code and exceptional
-            service to my clients. With two years of experience in web
-            development, I am confident in my ability to provide you with the
-            solutions you need to make your project a success.
+            Hi, my name is Mohamed Hassan Noor. I am Junior Full Stack Developer
+            with a solid foundation in JavaScript, React, and Ruby on Rails, I
+            am passionate about building intuitive and efficient web
+            applications. I am always on the lookout for opportunities to expand
+            my skills and take on innovative projects that push the boundaries
+            of these technologies.
           </p>
           <p>
-            I am a graduate of Microverse, an online coding school where I
-            completed Remote Full Stack Web Development Program, mastered
-            algorithms, data structures, and full-stack development with Ruby,
-            Rails, JavaScript, React, and Redux. I am Effective communicator and
-            collaborator with a strong work ethic. Ready to contribute as a
-            dedicated full-stack software developer.
+            I am seeking a position with a company that embodies the following
+            values:
+          </p>
+          <p>
+            - Innovative and Forward-thinking: I am drawn to environments that
+            are at the forefront of technology, continuously adapting and
+            evolving to set new industry standards.
+          </p>
+          <p>
+            - Team Collaboration: I value being part of a team where knowledge
+            sharing is encouraged, and each member actively contributes to
+            collective goals, fostering mutual growth and understanding.
+          </p>
+          <p>
+            - Opportunities for Growth: As a junior developer, I am eager to
+            work on projects that not only challenge me but also allow for
+            continuous learning and professional development.
+          </p>
+          <p>
+            I am open to joining companies of all scales, although I have a
+            particular interest in smaller organizations where I can make a
+            significant impact. While my expertise lies in web development, I am
+            open to exploring roles in various industries, always eager to
+            assimilate new knowledge and perspectives.
+          </p>
+          <p>
+            If your organization aligns with these values and you are looking
+            for a dedicated Developer skilled in JavaScript, React, and Ruby on
+            Rails, I would be thrilled to discuss potential opportunities with
+            you.
           </p>
           <a href='#contact' className='btn'>
             contact me
