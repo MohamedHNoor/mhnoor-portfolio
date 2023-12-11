4046b664f73a5765b68762c1757978db44ff180d
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-12-11 15:43:06 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,37 +20,9 @@ const About = () => {
             my skills and take on innovative projects that push the boundaries
             of these technologies.
           </p>
+          
           <p>
-            I am seeking a position with a company that embodies the following
-            values:
-          </p>
-          <p>
-            - Innovative and Forward-thinking: I am drawn to environments that
-            are at the forefront of technology, continuously adapting and
-            evolving to set new industry standards.
-          </p>
-          <p>
-            - Team Collaboration: I value being part of a team where knowledge
-            sharing is encouraged, and each member actively contributes to
-            collective goals, fostering mutual growth and understanding.
-          </p>
-          <p>
-            - Opportunities for Growth: As a junior developer, I am eager to
-            work on projects that not only challenge me but also allow for
-            continuous learning and professional development.
-          </p>
-          <p>
-            I am open to joining companies of all scales, although I have a
-            particular interest in smaller organizations where I can make a
-            significant impact. While my expertise lies in web development, I am
-            open to exploring roles in various industries, always eager to
-            assimilate new knowledge and perspectives.
-          </p>
-          <p>
-            If your organization aligns with these values and you are looking
-            for a dedicated Developer skilled in JavaScript, React, and Ruby on
-            Rails, I would be thrilled to discuss potential opportunities with
-            you.
+           If you need a skilled developer proficient in JavaScript, React, and Ruby on Rails who can positively impact your company's projects, look no further. Reach out to me through the following contact details.
           </p>
           <a href='#contact' className='btn'>
             contact me
